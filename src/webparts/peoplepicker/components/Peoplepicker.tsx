6c0ebded5cb2c36ce129355e3d0b101a343debc1
Maicon Ferreira <maicondevs@hotmail.com>
2021-02-06 00:03:52 -0300

    PEOPLE PICKER FINALIZADO

--- src/webparts/peoplepicker/components/Peoplepicker.tsx
+++ src/webparts/peoplepicker/components/Peoplepicker.tsx
@@ -1,25 +1,71 @@
-import * as React from 'react';
-import styles from './Peoplepicker.module.scss';
-import { IPeoplepickerProps } from './IPeoplepickerProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./Peoplepicker.module.scss";
+import { IPeoplepickerProps } from "./IPeoplepickerProps";
+import { NormalPeoplePicker, IPersonaProps, Label } from "office-ui-fabric-react";
 
-export default class Peoplepicker extends React.Component<IPeoplepickerProps, {}> {
-  public render(): React.ReactElement<IPeoplepickerProps> {
-    return (
-      <div className={ styles.peoplepicker }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Peoplepicker: React.FC<IPeoplepickerProps> = (props) => {
+  const [people, setPeople] = React.useState<any>([]);
+
+  const searchPeople = (
+    terms: string
+  ): IPersonaProps[] | Promise<IPersonaProps[]> =>
+    new Promise((resolve, reject) => {
+      fetch(
+        `https://solucoesspfx.sharepoint.com/_api/search/query?querytext='*${terms}*'&rowlimit=10&sourceid='b09a7990-05ea-4af9-81ef-edfab16c4e31'`,
+        {
+          headers: {
+            Accept: "application/json;odata=nometadata",
+            "odata-version": "",
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((response: { PrimaryQueryResult: any }) => {
+          let relevantResults: any =
+            response.PrimaryQueryResult.RelevantResults;
+          let resultCount: number = relevantResults.TotalRows;
+          let people = [];
+
+          if (resultCount > 0) {
+            relevantResults.Table.Rows.forEach(function (row) {
+              let persona: IPersonaProps = {};
+              row.Cells.forEach(function (cell) {
+                if (cell.Key === "PictureURL") persona.imageUrl = cell.Value;
+                if (cell.Key === "PreferredName")
+                  persona.primaryText = cell.Value;
+                if (cell.Key === "WorkEmail")
+                  persona.secondaryText = cell.Value;
+              });
+              people.push(persona);
+            });
+          }
+          resolve(people);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+
+  const getPeople = async (filterText: string) => {
+    if (filterText) {
+      if (filterText.length > 2) {
+        const search = await searchPeople(filterText);
+        setPeople(search);
+        return searchPeople(filterText);
+      }
+    } else {
+      return [];
+    }
+  };
+
+  return (
+    <div className={styles.peoplepicker}>
+      <Label>Peopler Picker</Label>
+      <NormalPeoplePicker onResolveSuggestions={getPeople} />
+    </div>
+  );
+};
+
+export default Peoplepicker;
